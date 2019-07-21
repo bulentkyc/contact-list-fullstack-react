ffc380f4108ce28a63186869e4e3147e9356b0c3
khadog <abodakhadog94@gmail.com>
2019-07-21 21:51:39 +0200

    update#9

--- src/Components/Dashboard/DashBoard.js
+++ src/Components/Dashboard/DashBoard.js
@@ -1,19 +1,48 @@
 import React from 'react';
-import classes from "./DashBoard.module.scss";
+import classes from './DashBoard.module.scss';
 
-class DashBoard extends React.Component{
-    render(){
-        return(
-            <section className={classes.dashBoard}>
-                <form action="/newContactList" >
-                    <input type="text" name="name"/>
-                    <input type="text" name="email"/>
-                    <input type="file" name="file"/>
-                    <button type="submit">Submit</button>
-                </form>
-            </section>
-        );
-    }
+class DashBoard extends React.Component {
+	render() {
+		return (
+			<div>
+				<section className={classes.containerForm}>
+					<img
+						className={classes.portfolioiImg}
+						src={require('../../assets/img/av.default.png')}
+						alt="portfolio"
+					/>
+					<form action="/newContactList">
+						<input type="text" name="name" />
+						<input type="text" name="email" />
+						<input type="file" name="file" />
+						<input type="submit" value="Submit" />
+					</form>
+				</section>
+				<section className={classes.contactList}>
+					<ul>
+						<li>
+							<img src={require('../../assets/img/av.default.png')} alt="portfolio" />
+						</li>
+						<li>Mohammad</li>
+						<li>Ahmad@gmail.com</li>
+						<li>
+							<img
+								className={classes.iconImg}
+								src={require('../../assets/img/user-edit-solid.svg')}
+								alt="edit"
+							/>{' '}
+						</li>
+						<li>
+							<img
+								className={classes.iconImg}
+								src={require('../../assets/img/times-solid.svg')}
+								alt="remove"
+							/>{' '}
+						</li>
+					</ul>
+				</section>
+			</div>
+		);
+	}
 }
-
 export default DashBoard;

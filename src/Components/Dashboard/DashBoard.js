5ef2a7a1f564991974d4aca5d2958086725b8e2a
khadog <abodakhadog94@gmail.com>
2019-07-22 16:28:08 +0200

    test!

--- src/Components/Dashboard/DashBoard.js
+++ src/Components/Dashboard/DashBoard.js
@@ -1,33 +1,35 @@
 import React from 'react';
 import classes from './DashBoard.module.scss';
 import Contact from './Contact/Contact';
-import axios from "axios";
+import axios from 'axios';
 import DefaultImg from '../../assets/img/av.default.png';
 
-
-class DashBoard extends React.Component{
-   state= {
-        userData: {},
-        contactList: [{id: 1},{id: 2}],
-        multerImage: DefaultImg,
-        failed: false,
-        errors: []
-}
-    nameRef= React.createRef();
-    emailRef= React.createRef();
-    componentDidMount(){
-        console.log(sessionStorage.getItem('loginStatus'));
-		if(sessionStorage.getItem('loginStatus') !== "true"){
-			// this.props.history.push('/login')
-		}else{
-            this.setState({userData: JSON.parse(sessionStorage.getItem('state'))})
-        }
-    }
-    validation(obj) {
+class DashBoard extends React.Component {
+	state = {
+		userData: {},
+		contactList: [
+			{ id: 1, name: 'Ahmad', email: 'ahmad@gmail.com' },
+			{ id: 2, name: 'Mohammad', email: 'mohammad@gmail.com' }
+		],
+		multerImage: DefaultImg,
+		failed: false,
+		errors: []
+	};
+	nameRef = React.createRef();
+	emailRef = React.createRef();
+	componentDidMount() {
+		console.log(sessionStorage.getItem('loginStatus'));
+		if (sessionStorage.getItem('loginStatus') !== 'true') {
+			this.props.history.push('/login');
+		} else {
+			this.setState({ userData: JSON.parse(sessionStorage.getItem('state')) });
+		}
+	}
+	validation(obj) {
 		let errors = [];
 		let isValid = true;
 		const emailReg = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
-        if (obj.name.trim().length < 2) {
+		if (obj.name.trim().length < 2) {
 			errors.push({ msg: 'Please enter the full name' });
 			isValid = false;
 		}
@@ -37,71 +39,82 @@ class DashBoard extends React.Component{
 		}
 		this.setState({ errors, failed: true });
 		return isValid;
-    }
-    uploadImage = (e)=>{
-        let imageFormObj = new FormData();
+	}
+	uploadImage = (e) => {
+		let imageFormObj = new FormData();
 
-        imageFormObj.append("imageName", "multer-image-" + Date.now());
-        imageFormObj.append("imageData", e.target.files[0]);
+		imageFormObj.append('imageName', 'multer-image-' + Date.now());
+		imageFormObj.append('imageData', e.target.files[0]);
 
-        // stores a readable instance of 
-        // the image being uploaded using multer
-        console.log(URL.createObjectURL(e.target.files[0]));
-        this.setState({
-            multerImage: URL.createObjectURL(e.target.files[0])
-        });
-    }
-    addContact = (e)=>{
-        e.preventDefault();
+		// stores a readable instance of
+		// the image being uploaded using multer
+		console.log(URL.createObjectURL(e.target.files[0]));
+		this.setState({
+			multerImage: URL.createObjectURL(e.target.files[0])
+		});
+	};
+	addContact = (e) => {
+		e.preventDefault();
 		let obj = {
-            userID: this.state.userData.id,
+			userID: this.state.userData.id,
 			name: this.nameRef.current.value,
-            email: this.emailRef.current.value,
-            avatar: this.state.multerImage
+			email: this.emailRef.current.value,
+			avatar: this.state.multerImage
 		};
 		if (this.validation(obj)) {
 			axios
 				.post('/newContact', { ...obj })
 				.then((response) => {
-					const contactList = [...response.data.contactList]
+					const contactList = [ ...response.data.contactList ];
 
 					console.log(contactList);
 					if (response.data.status === 'success') {
-                        this.setState({multerImage: DefaultImg, contactList,failed : false});
+						this.setState({ multerImage: DefaultImg, contactList, failed: false });
 					} else {
 						this.setState({ failed: true, errors: response.data.errors });
 					}
 				})
 				.catch((err) => {
-                    this.setState({multerImage: DefaultImg, errors: [ { msg: 'There Was a problem with server, Please try again later' } ] });
+					this.setState({
+						multerImage: DefaultImg,
+						errors: [ { msg: 'There Was a problem with server, Please try again later' } ]
+					});
 				});
 		}
-    }
+	};
+	updateContactList = () => {};
+	deleteContactList = () => {};
 	render() {
-        console.log(this.state)
+		console.log(this.state);
 		return (
-			<>
+			<div>
 				<section className={classes.containerForm}>
 					<img
-						className={classes.portfolioiImg}
+						className={classes.portfolioImg}
 						src={require('../../assets/img/av.default.png')}
 						alt="portfolio"
 					/>
-                    {this.state.errors.map((error, index) => (
-					<p className={classes.error} key={index}>
-						{error.msg}
-					</p>
-				    ))}
+					{this.state.errors.map((error, index) => (
+						<p className={classes.error} key={index}>
+							{error.msg}
+						</p>
+					))}
 					<form onSubmit={this.addContact}>
 						<input type="text" name="name" placeholder="Please enter contact name" ref={this.nameRef} />
-						<input type="text" name="email" placeholder="Please enter contact email" ref={this.emailRef}/>
-						<input type="file" name="file" onChange={(e) => this.uploadImage(e, "multer")}/>
+						<input type="text" name="email" placeholder="Please enter contact email" ref={this.emailRef} />
+						<input type="file" name="file" onChange={this.uploadImage} />
 						<input type="submit" value="Submit" />
 					</form>
 				</section>
-                {this.state.contactList.map(item => <Contact key={item.id} {...item} />)}
-				
-			</>
+				{this.state.contactList.map((item) => (
+					<Contact
+						key={item.id}
+						{...item}
+						updateContactList={this.updateContactList}
+						deleteContactList={this.deleteContactList}
+					/>
+				))}
+			</div>
 		);
 	}
 }

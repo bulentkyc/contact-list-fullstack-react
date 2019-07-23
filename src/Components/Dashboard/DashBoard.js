a54efb007a4c28268c7ce28667bb5c1f6596b1b5
khadog <abodakhadog94@gmail.com>
2019-07-23 14:31:37 +0200

    test

--- src/Components/Dashboard/DashBoard.js
+++ src/Components/Dashboard/DashBoard.js
@@ -73,6 +73,24 @@ class DashBoard extends React.Component {
 		if (this.validation(obj)) {
 			if (this.state.selectedFile === null) {
 				obj.avatar = 'av.default.png';
+				axios
+					.post('/newContact', { ...obj })
+					.then((res) => {
+						console.log(res);
+						const newContact = res.data.newContactData;
+						let contactList = [ ...this.state.contactList ];
+						contactList.push(newContact);
+						if (res.data.status === 'success') {
+							this.setState({ selectedFile: null, contactList: contactList, failed: false, errors: [] });
+						} else {
+							errors = [ ...res.data.errors ];
+							this.setState({ failed: true, errors });
+						}
+					})
+					.catch((err) => {
+						errors.push({ msg: 'There Was a problem with server, Please try again later' });
+						this.setState({ selectedFile: null, errors });
+					});
 			} else {
 				const data = new FormData();
 				data.append('file', this.state.selectedFile);
@@ -119,31 +137,14 @@ class DashBoard extends React.Component {
 						}
 					});
 			}
-			axios
-				.post('/newContact', { ...obj })
-				.then((res) => {
-					console.log(res);
-					const newContact = res.data.newContactData;
-					let contactList = [ ...this.state.contactList ];
-					contactList.push(newContact);
-					if (res.data.status === 'success') {
-						this.setState({ selectedFile: null, contactList: contactList, failed: false, errors: [] });
-					} else {
-						errors = [ ...res.data.errors ];
-						this.setState({ failed: true, errors });
-					}
-				})
-				.catch((err) => {
-					errors.push({ msg: 'There Was a problem with server, Please try again later' });
-					this.setState({ selectedFile: null, errors });
-				});
+
 			e.target.reset();
 		}
 	};
 	updateContactList = () => {};
 	deleteContactList = () => {};
 	sendEmail = () => {
-		this.setState({ sendEmail: !this.state.sendEmailOpen });
+		this.setState({ sendEmailOpen: !this.state.sendEmailOpen });
 	};
 	render() {
 		return (
